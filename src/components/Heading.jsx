fc667cb964e25ba6f703b68abeca919d3ee2544c
Aashish17405 <aashish17405@gmail.com>
2024-06-23 19:09:36 +0530

    updated codes

--- src/components/Heading.jsx
+++
@@ -1,5 +0,0 @@
-export default function Heading(){
-    return <div>
-        <h1 className='ml-20 mt-20 font-extrabold text-2xl sm:ml-10 sm:mt-24 sm:font-extrabold sm:text-5xl'>Todo Application</h1>
-    </div>
-}
