6818d1a2b4257f2e012d94d8539b92de832bf6b3
Aashish17405 <aashish17405@gmail.com>
2024-08-26 14:05:55 +0530

    .

--- src/components/createTodos.jsx
+++ src/components/createTodos.jsx
@@ -39,8 +39,7 @@ function CreateTodos() {
 
       const json = await response.json();
       toast.success("Todo added successfully");
-      setTodo((todo) => [...todo, newTodo]);
-      setTodo("");
+      setTodo(""); // Clear the input after successful addition
     } catch (error) {
       toast.error("Failed to add todo");
       console.error("There was an error:", error);
@@ -53,29 +52,32 @@ function CreateTodos() {
     setTodo(e.target.value);
   }
 
-  function handleSubmit() {
+  function handleSubmit(e) {
+    e.preventDefault();
     addTodo(todo);
   }
 
   return (
     <>
       <div className='my-8 flex justify-center sm:mt-10 sm:ml-3'>
-        <input
-          className="px-5 py-2 bg-black sm:w-96 sm:h-12 sm:py-2 sm:px-6 sm:ml-0 sm:m-2 rounded-full border border-gray-300"
-          type='text'
-          placeholder='Todo'
-          value={todo}
-          onChange={handleChange}
-        />
-        <button
-          className="border border-gray-300 ml-2 rounded-full px-2 sm:bg-black sm:hover:bg-blue sm:hover:text-black sm:px-3 sm:rounded-full sm:mt-2 sm:h-12"
-          onClick={()=>{handleSubmit()}}
-          disabled={isLoading}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-          </svg>
-        </button>
+        <form onSubmit={handleSubmit} className="flex">
+          <input
+            className="px-5 py-2 bg-black sm:w-96 sm:h-12 sm:py-2 sm:px-6 sm:ml-0 sm:m-2 rounded-full border border-gray-300"
+            type='text'
+            placeholder='Todo'
+            value={todo}
+            onChange={handleChange}
+          />
+          <button
+            className="border border-gray-300 ml-2 rounded-full px-2 sm:bg-black sm:hover:bg-blue sm:hover:text-black sm:px-3 sm:rounded-full sm:mt-2 sm:h-12"
+            type="submit"
+            disabled={isLoading}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
+          </button>
+        </form>
         {isLoading && (
           <div
             className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50"
