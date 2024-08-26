f02cb3303350e4fc47c539c7f417458322890720
Aashish17405 <aashish17405@gmail.com>
2024-08-26 14:02:13 +0530

    .

--- src/components/createTodos.jsx
+++ src/components/createTodos.jsx
@@ -22,7 +22,7 @@ function CreateTodos() {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://localhost:3001/add", {
+      const response = await fetch("https://todo99x.onrender.com/add", {
         method: "POST",
         headers: {
           'Content-type': "application/json",
